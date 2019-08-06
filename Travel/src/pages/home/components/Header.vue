199a9ee2b14c33119dcda69aa06db085fdb60dda
xinmos <xiongxinq@outlook.com>
2019-08-06 19:01:41 +0800

    导航栏实现：增加iconfont图片

---
+++ Travel/src/pages/home/components/Header.vue
@@ -0,0 +1,52 @@
+<template>
+	<div>
+		<div class="header">
+			<div class="header-left">
+				<span class="iconfont" back-icon>&#xe658;</span>
+			</div>
+			<div class="header-input">
+				<span class="iconfont">&#xe63c;</span>
+				输入景点/城市/游玩主题
+			</div>
+			<div class="header-right">
+				城市
+				<span class="iconfont" arrow-icon>&#xe65c;</span>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script type="text/javascript">
+	export default {
+		name: 'HomeHeader'
+	}
+</script>
+
+<style lang="stylus" scoped> 
+	@import '~styles/varibles.styl'
+	.header
+		display: flex
+		line-height: 43px
+		background: $bgColor
+		color: #fff
+		.header-left
+			width: 32px
+			float: left
+			.back-icon
+				text-align: center
+		.header-input
+			flex: 1
+			height: 38px
+			line-height: 38px
+			margin-top: 3px
+			margin-left: 5px
+			background: #fff
+			border-radius: 4px
+			color: #ccc
+		.header-right
+			width: 62px
+			float: right
+			text-align: center
+			.arrow-icon
+				text-align: center
+</style>
