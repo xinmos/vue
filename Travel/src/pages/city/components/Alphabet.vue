e905bb5a26cadc59985bfa19700b29471d1a24f8
xinmos <xiongxinq@outlook.com>
2019-08-07 21:31:13 +0800

    添加better-scroll插件，完成城市列表模型

---
+++ Travel/src/pages/city/components/Alphabet.vue
@@ -0,0 +1,32 @@
+<template>
+	<ul class="list">
+		<li class="item">A</li>
+		<li class="item">A</li>
+		<li class="item">A</li>
+		<li class="item">A</li>
+		<li class="item">A</li>
+		<li class="item">A</li>
+	</ul>
+</template>
+
+<script type="text/javascript">
+export default {
+	name: 'CityAlphabet'
+}
+</script>
+
+<style lang="stylus" scoped>
+	@import '~styles/varibles.styl'
+	.list
+		display: flex
+		flex-direction: column
+		justify-content: center
+		position: absolute
+		top: 296px
+		bottom: 0
+		width: 16px
+		right: 0
+		.item
+			text-align: center
+			line-height: 28px
+</style>
---
+++ Travel/src/pages/city/components/List.vue
@@ -0,0 +1,123 @@
+<template>
+	<div class="list" ref="wrapper">
+		<div>
+			<div class="area">
+				<div class="title boder-topbottom">当前城市</div>
+				<div class="button-list">
+					<div class="button-wrapper">
+						<div class="button">北京</div>
+					</div>
+				</div>
+			</div>
+			<div class="area">
+				<div class="title boder-topbottom">热门城市</div>
+				<div class="button-list">
+					<div class="button-wrapper">
+						<div class="button">北京</div>
+					</div>
+					<div class="button-wrapper">
+						<div class="button">北京</div>
+					</div>
+					<div class="button-wrapper">
+						<div class="button">北京</div>
+					</div>
+					<div class="button-wrapper">
+						<div class="button">北京</div>
+					</div>
+					<div class="button-wrapper">
+						<div class="button">北京</div>
+					</div>
+				</div>
+			</div>
+			<div class="area">
+				<div class="title boder-topbottom">A</div>
+				<div class="item-list">
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+				</div>
+				<div class="title boder-topbottom">A</div>
+				<div class="item-list">
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+				</div>
+				<div class="title boder-topbottom">A</div>
+				<div class="item-list">
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+				</div>
+				<div class="title boder-topbottom">A</div>
+				<div class="item-list">
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script type="text/javascript">
+import BScroll from 'better-scroll'
+export default {
+	name: 'CityList',
+	mounted () {
+		this.scroll = new BScroll(this.$refs.wrapper)
+	}
+}
+</script>
+
+<style lang="stylus" scoped>
+	@import '~styles/varibles.styl'
+	.boder-topbottom
+		&:before
+			border-color: #ccc
+		&:after
+			border-color: #ccc
+	.border-bottom
+		&:before
+			border-color: #ccc
+	.list
+		overflow: hidden
+		position: absolute
+		top: 98px
+		left 0
+		right: 0
+		bottom: 0
+		.title
+			line-height: 24px
+			background: #eee
+			padding-left: 10px 
+			color: #666
+			font-size: 12px
+		.button-list
+			overflow: hidden
+			padding: 6px 36px 6px 6px
+			.button-wrapper
+				float: left
+				width: 33.33%
+				.button
+					margin: 4px
+					padding: 3px 0
+					text-align: center
+					border: 2px solid #ccc
+					border-radius: 5px
+		.item-list
+			.item
+				line-height: 40px
+				padding-left: 8px
+</style>
