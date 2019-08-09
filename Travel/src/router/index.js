e579982028abcf7ad6428080010877397bcf3832
xinmos <xiongxinq@outlook.com>
2019-08-09 20:11:04 +0800

    异步按需加载文件

--- Travel/src/router/index.js
+++ Travel/src/router/index.js
@@ -1,9 +1,6 @@
 // 路由配置文件
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/pages/home/Home'
-import City from '@/pages/city/City'
-import Detail from '@/pages/detail/Detail'
 
 Vue.use(Router)
 
@@ -12,15 +9,15 @@ export default new Router({
     {
       path: '/',
       name: 'Home',
-      component: Home
+      component: () => import('@/pages/home/Home')
     },{
       path: '/city',
       name: 'City',
-      component: City
+      component: () => import('@/pages/city/City')
     },{
       path: '/detail/:id',
       name: 'Detail',
-      component: Detail
+      component: () => import('@/pages/detail/Detail')
     }
   ],
   scrollBehavior (to, from, savedPosition) {
